bd8632d959f850f5ea5b7e40c3580af4b01b77c8
Syed Ayaz <51998832+syedayazsa@users.noreply.github.com>
2022-04-21 03:24:37 +0530

    Time: 141 ms (83.07%), Space: 83.4 MB (38.98%) - LeetHub

--- 775-global-and-local-inversions/775-global-and-local-inversions.cpp
+++ 775-global-and-local-inversions/775-global-and-local-inversions.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool isIdealPermutation(vector<int>& nums) {
-        int n = nums.size();
-        if(n == 0 or n == 1 or n == 2) return true;
-        
-        int maximum = -1;
-        for(int i = 0; i <= nums.size() - 3; i++){
-            maximum = max(maximum, nums[i]);
-            if(maximum > nums[i + 2]) return false;
+        int cmax = 0, n = nums.size();
+        for (int i = 0; i < n - 2; ++i) {
+            cmax = max(cmax, nums[i]);
+            if (cmax > nums[i + 2]) return false;
         }
-            
         return true;
     }
 };
