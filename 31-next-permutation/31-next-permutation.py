9774dad376ced4ef356ad4689ce3d872a6ef17c0
Syed Ayaz <51998832+syedayazsa@users.noreply.github.com>
2022-07-16 12:28:41 +0300

    Time: 77 ms (25.85%), Space: 13.9 MB (24.65%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -3,29 +3,26 @@ class Solution:
         
         N = len(nums)
         pivot = N-2
-        swap = N-1
         
-        # while pivot >= 0:
-        #     if nums[pivot] <= nums[pivot+1]:
-        #         break
-        #     pivot -= 1
-        
+        #finding pivot
         for pivot in range(N-2, -2, -1):
             if nums[pivot] < nums[pivot+1]:
                 break
         
-        print(pivot)
-        
+        #corner case Eg: [3,2,1]
         if pivot == -1:
             nums.reverse()
             return
         
+        # finding element to swap with pivot
+        swap = N-1
         for swap in range(N-1, pivot, -1):
             if nums[swap] > nums[pivot]:
                 break;
                 
-        print(swap)
-        
+        #swapping pivot, swap and reversing after pivot
         nums[pivot], nums[swap] = nums[swap], nums[pivot]
         nums[pivot+1:] = nums[pivot+1:][::-1]
+        
+        
         
